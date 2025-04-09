3bbef0a2dbdd547090da8126f8ab466e6d40635b
Ryan Mathews <MathewsThomasRyan@gmail.com>
2025-04-09 15:46:45 -0500

    add quit message

--- GuessGame.py
+++ GuessGame.py
@@ -10,18 +10,18 @@ while playerGuess != randNum:
 
     if playerGuess < randNum:
         guesses + 1
-        playerGuess = int(input("Too low! Try again: "))
+        playerGuess = int(input("Too low! Try again or type 'exit' to quit: "))
 
     elif playerGuess > randNum:
         guesses + 1
-        playerGuess = int(input("Too high! Try again: "))
+        playerGuess = int(input("Too high! Try again or type 'exit' to quit: "))
 
     elif playerGuess == randNum:
         guesses + 1
         print(f"You guessed the correct number in {guesses} guesses!")
 
         randNum = random.randint(1, 9)
-        playerGuess = int(input("Guess the random number: "))
+        playerGuess = int(input("Guess the random number or 'exit' to quit: "))
 
     elif playerGuess == "exit":
         exit
