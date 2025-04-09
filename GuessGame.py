9da8e49881cecc362d33536286069bde06e0b00a
Ryan Mathews <MathewsThomasRyan@gmail.com>
2025-04-09 15:33:02 -0500

    indent

--- GuessGame.py
+++ GuessGame.py
@@ -6,14 +6,16 @@ guesses = 0
 
 playerGuess = int(input("Guess the random numer: "))
 
-if playerGuess < randNum:
-    guesses + 1
-    print("Too low! Try again: ")
+while playerGuess != randNum:
 
-if playerGuess > randNum:
-    guesses + 1
-    print("Too high! Try again: ")
+    if playerGuess < randNum:
+        guesses + 1
+        print("Too low! Try again: ")
 
-if playerGuess == randNum:
-    guesses + 1
-    print(f"You guessed the correct number in {guesses} guesses!")
+    if playerGuess > randNum:
+        guesses + 1
+        print("Too high! Try again: ")
+
+    if playerGuess == randNum:
+        guesses + 1
+        print(f"You guessed the correct number in {guesses} guesses!")
