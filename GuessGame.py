8156ad68ab67472f193ef837b9f68f6f4a943540
Ryan Mathews <MathewsThomasRyan@gmail.com>
2025-04-09 15:24:29 -0500

    add if too low / if too high

--- GuessGame.py
+++ GuessGame.py
@@ -2,3 +2,10 @@ import random
 
 randNum = random.randint(1, 9)
 
+playerGuess = input("Guess the random numer: ")
+
+if playerGuess < randNum:
+    print("Too low! Try again: ")
+
+if playerGuess > randNum:
+    print("Too high! Try again: ")
