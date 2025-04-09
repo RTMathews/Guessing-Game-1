2571a2152c7fd560fe5baf0f701a36de8dba8aeb
Ryan Mathews <MathewsThomasRyan@gmail.com>
2025-04-09 15:26:54 -0500

    add guesses counter

--- GuessGame.py
+++ GuessGame.py
@@ -2,10 +2,18 @@ import random
 
 randNum = random.randint(1, 9)
 
+guesses = 0
+
 playerGuess = input("Guess the random numer: ")
 
 if playerGuess < randNum:
+    guesses + 1
     print("Too low! Try again: ")
 
 if playerGuess > randNum:
+    guesses + 1
     print("Too high! Try again: ")
+
+if playerGuess == randNum:
+    guesses + 1
+    print("You guess the correct number!")
