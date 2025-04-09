6fb676306bdb1a87d9c94862dcffae71f2f5fc15
Ryan Mathews <MathewsThomasRyan@gmail.com>
2025-04-09 15:30:40 -0500

    make playerGuess int

--- GuessGame.py
+++ GuessGame.py
@@ -4,7 +4,7 @@ randNum = random.randint(1, 9)
 
 guesses = 0
 
-playerGuess = input("Guess the random numer: ")
+playerGuess = int(input("Guess the random numer: "))
 
 if playerGuess < randNum:
     guesses + 1
@@ -17,4 +17,3 @@ if playerGuess > randNum:
 if playerGuess == randNum:
     guesses + 1
     print(f"You guessed the correct number in {guesses} guesses!")
-
