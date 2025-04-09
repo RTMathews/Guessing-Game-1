a7ae08a8bb13672df966b2709c221c52001e80ad
Ryan Mathews <MathewsThomasRyan@gmail.com>
2025-04-09 16:23:51 -0500

    exit working now

--- GuessGame.py
+++ GuessGame.py
@@ -4,26 +4,24 @@ randNum = random.randint(1, 9)
 
 guesses = 0
 
-playerGuess = int(input("Guess the random number: "))
+playerGuess = input("Guess the random number: ")
 
 while playerGuess != randNum:
 
-    if playerGuess < randNum:
-        guesses + 1
-        playerGuess = int(input("Too low! Try again or type 'exit' to quit: "))
+    if playerGuess.lower() == "exit":
+        exit()
 
-    elif playerGuess > randNum:
+    if int(playerGuess) < randNum:
         guesses + 1
-        playerGuess = int(input("Too high! Try again or type 'exit' to quit: "))
+        playerGuess = input("Too low! Try again or type 'exit' to quit: ")
 
-    elif playerGuess == randNum:
+    if int(playerGuess) > randNum:
+        guesses + 1
+        playerGuess = input("Too high! Try again or type 'exit' to quit: ")
+
+    if int(playerGuess) == randNum:
         guesses + 1
         print(f"You guessed the correct number in {guesses} guesses!")
 
         randNum = random.randint(1, 9)
-        playerGuess = int(input("Guess the random number or 'exit' to quit: "))
-
-    elif playerGuess == "exit":
-        exit
-
-    
+        playerGuess = input("Guess the random number or type 'exit' to quit: ")
